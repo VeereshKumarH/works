66f94f59163874eb367284d7494de08b9f5ab92b
VeereshKumarH <72198751+VeereshKumarH@users.noreply.github.com>
2025-03-16 00:15:17 +0530

    API Change Commit
    Signed-off-by: VeereshKumarH <72198751+VeereshKumarH@users.noreply.github.com>

--- RAG/streamlit_app.py
+++ RAG/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "https://0d1d-35-230-11-195.ngrok-free.app/predict"
+API_URL = "https://e8be-34-87-66-145.ngrok-free.app/predict"
 
 st.title("Financial RAG Chatbot")
 st.write("Ask financial questions based on company earnings reports!")
